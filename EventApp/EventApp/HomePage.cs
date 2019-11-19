e1ca5aea7d3780b00a94e582ef3b87123df5d440
Zisis Spatis <31807214+zisispa@users.noreply.github.com>
2019-11-19 22:13:44 +0200

    new with - Create Event - Home - About Us

--- EventApp/EventApp/HomePage.cs
+++ EventApp/EventApp/HomePage.cs
@@ -15,36 +15,110 @@ namespace EventApp
         public HomePage()
         {
             InitializeComponent();
+            SidePanel.Height = Create_Event.Height;
+            userControlCreateEvent1.BringToFront();
+            SidePanel.Height = Home.Height;
+            userControlΗοme1.BringToFront();
+            userControlAboutUs1.BringToFront();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Home_MouseHover(object sender, EventArgs e)
         {
-
+            Home.BackColor = Color.FromArgb(85, 61, 69);  //allagei xrwma otan pernas to pontiki panw apto koumpi 
+            Home.ForeColor = Color.Black;
         }
-
-        private void label2_Click(object sender, EventArgs e)
+        
+        private void Home_MouseLeave(object sender, EventArgs e)
         {
-
+            Home.BackColor = Color.FromArgb(87, 96, 122);  //epanafora tou xrwmatos otan fygei to pontiki
+            Home.ForeColor = Color.Snow;
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Home_Click(object sender, EventArgs e)
         {
-
+            SidePanel.Height = Home.Height;
+            SidePanel.Top = Home.Top;
+            userControlΗοme1.BringToFront();
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void Create_Event_Click(object sender, EventArgs e)
         {
-
+            SidePanel.Height = Create_Event.Height;
+            SidePanel.Top = Create_Event.Top;
+            userControlCreateEvent1.BringToFront();
         }
 
-        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        private void Create_Event_MouseHover_1(object sender, EventArgs e)
         {
+            Create_Event.BackColor = Color.FromArgb(85, 61, 69);  //allagei xrwma otan pernas to pontiki panw apto koumpi 
+            Create_Event.ForeColor = Color.Black;
+        }
+        private void Create_Event_MouseLeave(object sender, EventArgs e)
+        {
+            Create_Event.BackColor = Color.FromArgb(87, 96, 122);  //epanafora tou xrwmatos otan fygei to pontiki
+            Create_Event.ForeColor = Color.Snow;
+        } 
 
+        private void Help_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = Help.Height;
+            SidePanel.Top = Help.Top;
+        }
+        
+        private void Help_MouseHover(object sender, EventArgs e)
+        {
+            Help.BackColor = Color.FromArgb(85, 61, 69);  //allagei xrwma otan pernas to pontiki panw apto koumpi 
+            Help.ForeColor = Color.Black;
         }
 
-        private void Logout_Click(object sender, EventArgs e)
+        private void Help_MouseLeave(object sender, EventArgs e)
+        {
+            Help.BackColor = Color.FromArgb(87, 96, 122);  //epanafora tou xrwmatos otan fygei to pontiki
+            Help.ForeColor = Color.Snow;
+        }
+        
+        private void About_Us_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = About_Us.Height;
+            SidePanel.Top = About_Us.Top;
+            userControlAboutUs1.BringToFront();
+        }
+
+        private void About_Us_MouseHover(object sender, EventArgs e)
+        {
+            About_Us.BackColor = Color.FromArgb(85, 61, 69);   
+            About_Us.ForeColor = Color.Black;
+        }
+        
+        private void About_Us_MouseLeave(object sender, EventArgs e)
+        {
+            About_Us.BackColor = Color.FromArgb(87, 96, 122);  
+            About_Us.ForeColor = Color.Snow;
+        }
+
+        private void Contact_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = Contact.Height;
+            SidePanel.Top = Contact.Top;
+        }
+
+        private void Contact_MouseHover(object sender, EventArgs e)
+        {
+            Contact.BackColor = Color.FromArgb(85, 61, 69);
+            Contact.ForeColor = Color.Black;
+        }
+
+        private void Contact_MouseLeave(object sender, EventArgs e)
+        {
+            Contact.BackColor = Color.FromArgb(87, 96, 122);
+            Contact.ForeColor = Color.Snow;
+        }
+
+        private void pictureBoxExit_Click(object sender, EventArgs e)
         {
             Application.Exit(); // Kleinei tin Efarmogh
         }
+
+        
     }
 }
