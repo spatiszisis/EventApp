dcc8a953a39a51a1d29b7e21587908d957b55f32
zisispatis <z_is97@hotmail.com>
2019-12-29 16:16:01 +0200

    last version

---
+++ EventApp/EventApp/UserControlHelp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventApp
+{
+    public partial class UserControlHelp : UserControl
+    {
+        public UserControlHelp()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControlHelp_Load(object sender, EventArgs e)
+        {
+            //Dark Mode
+            int c = (int)UserControlSettingsApp.color;
+            if (c == 0)
+            {
+                this.BackColor = Color.Black;
+            }
+            else if (c == 1)
+            {
+                this.BackColor = Color.White;
+            }
+            //Grammatoseira
+            int g = (int)UserControlSettingsApp.gram;
+            if (g == 0)
+            {
+                label4.Font = new Font("Arial", 16, FontStyle.Bold);
+                label17.Font = new Font("Arial", 16, FontStyle.Bold);
+                label6.Font = new Font("Arial", 16, FontStyle.Bold);
+                label18.Font = new Font("Arial", 11, FontStyle.Bold);
+                label19.Font = new Font("Arial", 11, FontStyle.Bold);
+                label20.Font = new Font("Arial", 11, FontStyle.Bold);
+                label21.Font = new Font("Arial", 11, FontStyle.Bold);
+                label22.Font = new Font("Arial", 11, FontStyle.Bold);
+                label23.Font = new Font("Arial", 11, FontStyle.Bold);
+                label13.Font = new Font("Arial", 11, FontStyle.Regular);
+                label12.Font = new Font("Arial", 11, FontStyle.Regular);
+                label11.Font = new Font("Arial", 11, FontStyle.Regular);
+                label10.Font = new Font("Arial", 11, FontStyle.Regular);
+                label5.Font = new Font("Arial", 11, FontStyle.Regular);
+                label7.Font = new Font("Arial", 11, FontStyle.Regular);
+                label9.Font = new Font("Arial", 11, FontStyle.Regular);
+                label14.Font = new Font("Arial", 11, FontStyle.Regular);
+                label15.Font = new Font("Arial", 11, FontStyle.Regular);
+                label16.Font = new Font("Arial", 11, FontStyle.Regular);
+            }
+            else if (g == 1)
+            {
+                label4.Font = new Font("Arial", 12, FontStyle.Bold);
+                label6.Font = new Font("Arial", 12, FontStyle.Bold);
+                label17.Font = new Font("Arial", 12, FontStyle.Bold);
+                label18.Font = new Font("Arial", 9, FontStyle.Bold);
+                label19.Font = new Font("Arial", 9, FontStyle.Bold);
+                label20.Font = new Font("Arial", 9, FontStyle.Bold);
+                label21.Font = new Font("Arial", 9, FontStyle.Bold);
+                label22.Font = new Font("Arial", 9, FontStyle.Bold);
+                label23.Font = new Font("Arial", 9, FontStyle.Bold);
+                label13.Font = new Font("Arial", 9, FontStyle.Regular);
+                label12.Font = new Font("Arial", 9, FontStyle.Regular);
+                label11.Font = new Font("Arial", 9, FontStyle.Regular);
+                label10.Font = new Font("Arial", 9, FontStyle.Regular);
+                label5.Font = new Font("Arial", 9, FontStyle.Regular);
+                label7.Font = new Font("Arial", 9, FontStyle.Regular);
+                label9.Font = new Font("Arial", 9, FontStyle.Regular);
+                label14.Font = new Font("Arial", 9, FontStyle.Regular);
+                label15.Font = new Font("Arial", 9, FontStyle.Regular);
+                label16.Font = new Font("Arial", 9, FontStyle.Regular);
+            }
+        }
+
+    }
+}
---
+++ EventApp/EventApp/UserControlManage.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace EventApp
+{
+    partial class UserControlManage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.dataGridViewEvents = new System.Windows.Forms.DataGridView();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(61)))), ((int)(((byte)(69)))));
+            this.label2.Location = new System.Drawing.Point(38, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 58);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Manage";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(61)))), ((int)(((byte)(69)))));
+            this.label3.Location = new System.Drawing.Point(45, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(712, 17);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "_________________________________________________________________________________" +
+    "_______";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(61)))), ((int)(((byte)(69)))));
+            this.label1.Location = new System.Drawing.Point(42, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 46);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Users";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(61)))), ((int)(((byte)(69)))));
+            this.label4.Location = new System.Drawing.Point(42, 452);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 46);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Events";
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(48, 216);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.RowHeadersWidth = 51;
+            this.dataGridViewUsers.RowTemplate.Height = 24;
+            this.dataGridViewUsers.Size = new System.Drawing.Size(1171, 150);
+            this.dataGridViewUsers.TabIndex = 28;
+            // 
+            // dataGridViewEvents
+            // 
+            this.dataGridViewEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvents.Location = new System.Drawing.Point(50, 501);
+            this.dataGridViewEvents.Name = "dataGridViewEvents";
+            this.dataGridViewEvents.RowHeadersWidth = 51;
+            this.dataGridViewEvents.RowTemplate.Height = 24;
+            this.dataGridViewEvents.Size = new System.Drawing.Size(1169, 150);
+            this.dataGridViewEvents.TabIndex = 29;
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.delete_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(61)))), ((int)(((byte)(69)))));
+            this.delete_btn.FlatAppearance.BorderSize = 0;
+            this.delete_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.delete_btn.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold);
+            this.delete_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.delete_btn.Location = new System.Drawing.Point(50, 689);
+            this.delete_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(208, 53);
+            this.delete_btn.TabIndex = 31;
+            this.delete_btn.Text = "Delete";
+            this.delete_btn.UseVisualStyleBackColor = false;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.delete_btn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dataGridViewEvents);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dataGridViewUsers);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1453, 785);
+            this.panel1.TabIndex = 32;
+            // 
+            // UserControlManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "UserControlManage";
+            this.Size = new System.Drawing.Size(1453, 785);
+            this.Load += new System.EventHandler(this.UserControlManage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.DataGridView dataGridViewEvents;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
